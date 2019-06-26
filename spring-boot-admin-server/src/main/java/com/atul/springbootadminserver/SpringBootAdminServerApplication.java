6191fd7776bb176b9a81629c3581d88ab75e6bfa
Atul <IamAtulMGupta@gmail.com>
2019-06-25 20:48:50 -0700

    updated properties

--- spring-boot-admin-server/src/main/java/com/atul/springbootadminserver/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/com/atul/springbootadminserver/SpringBootAdminServerApplication.java
@@ -3,6 +3,8 @@ package com.atul.springbootadminserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminServerApplication {
 
